97fdaee0d31a4a61708bf766394a7a3075814775
swetha-200160 <dhanyadhaya590@gmail.com>
2025-10-09 21:48:27 +0530

    Update write.py

--- write.py
+++ write.py
@@ -1,1 +1,12 @@
-print("Hello, Jenkins! This is a simple Python script.")
+# Sample Python program
+
+# Print a greeting
+print("Hello, World!")
+
+# Add two numbers
+a = 5
+b = 7
+sum = a + b
+
+print("The sum of", a, "and", b, "is", sum)
+
